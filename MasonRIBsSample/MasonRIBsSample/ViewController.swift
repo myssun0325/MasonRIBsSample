5980bb3aac864ec58cee1ec37e53b0117cde28c5
mason <mason@masonui-MacBookPro.local>
2022-02-27 13:27:28 +0900

    add gitignore

---
+++ MasonRIBsSample/MasonRIBsSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasonRIBsSample
+//
+//  Created by mason on 2022/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
